a1664b96973e5abddc207f6fe545dc4f281a4e93
rafaelcariciopereira <csrafa34@gmail.com>
2023-10-16 07:48:30 -0300

    fsadsa

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const mongoose = require("mongoose") 
 const express = require('express')
-const mongoose = require('mongoose');
 const cors = require('cors')
 const tableList = require('./tableList');
 
-const port = 5000
-const uri = "mongodb+srv://devcegliapedro:NH16WsNQbh4ahnpH@clusterpctx.ag1rmhm.mongodb.net/?retryWrites=true&w=majority";
+const port = 5001
+const uri = "mongodb+srv://pctxdev:felinharcp2005@cluster0.xcxso7v.mongodb.net/?retryWrites=true&w=majority";
+
+//require("dotenv").config()
 
 const app = express()
 
@@ -23,7 +25,10 @@ app.get('/estoque', (req, res) => {
     res.json(tableList)
   })
 
+  
 app.listen(port, () => {
     console.log("Start Server: Rodando na Porta " + port)
-    mongoose.connect(uri).then(()=>{console.log("Rodou!")}).catch((erro)=>{console.log(erro)});
+    mongoose.connect(uri).then(()=>{console.log("MongoDB CONECTADO!")}).catch((erro)=>{console.log(erro)});
 })
+
+
